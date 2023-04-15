cee10f3ccabc61e67a5aa88fc0b253852630420b
unknown <arixxdelera21@gmail.com>
2023-04-16 00:44:12 +0800

    adding deisngs and finalization of code

--- decryption.py
+++ decryption.py
@@ -1,14 +1,26 @@
 # Delera, Aritz B.
 # decryptor
 
+# import pyfiglet module
+import pyfiglet
+
+opening = pyfiglet.figlet_format("= O.O.P =", font = "lean")
+print(opening)
+
+
+# Create an introduction
+print("=" * 51)
+print("\033[32m Welcome to AritzMetic's Decrypter! \033[0m".center(60, "+"))
+print("=" * 51)
+
 # Use while loop for a condition-controlled loop
 while True:
     # Ask the name of the user to create a greeting
-    name = input("Hi Smart Pipol! what is your name?")
-    print("Hi", name, "! AritzMetic is here to help you in decrypting your code!")
+    name = input("\033[34mHi Smart Pipol! what is your name?\033[0m")
+    print("\033[40mHi", name, "! AritzMetic is here to help you in decrypting your code!\033[0m")
 
     # ask the user to input the encrypted text
-    user_encryptedtxt = input("What is the text that you want to decrypt?: ")
+    user_encryptedtxt = input("\033[31mWhat is the text that you want to decrypt?: \033[0m")
 
     # Make a decryption class that we will be using for the program
     class Decryption:
@@ -46,11 +58,11 @@ while True:
     # use the decrypter() method and save the decrypted text
     inputted_text = decrypt.decrypter()
     # print out the decrypted text
-    print("Yown! The process has been completed!", inputted_text,"is your plain text!")
+    print("\033[40mYown! The process has been completed!", inputted_text,"is your plain text!\033[0m")
     # Ask the user if they want to repeat the process
-    question = input("Would you like to repeat the process? (yes or no): ")
+    question = input("\033[36mWould you like to repeat the process? (yes or no): \033[0m")
     # If the user doesn't want to repeat the process, exit the loop
     if question.lower() == "no":
-        closing = "Thank you for using AritzMetic's Decrypter. Have a nice day!"
+        closing = pyfiglet.figlet_format("Thank you for using AritzMetic's Decrypter. Have a nice day!", font="puffy")
         print(closing)
         quit()
