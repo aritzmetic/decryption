e1a4c38ed3c2535fbf85b1966b2cdf7355dfa200
unknown <arixxdelera21@gmail.com>
2023-04-15 23:26:22 +0800

    pseudocode has been added

---
+++ decryption.py
@@ -0,0 +1,22 @@
+# Delera, Aritz B.
+# decryptor
+
+# Create an introduction
+# Use while loop for a condition-controlled loop
+# Ask the name of the user to create a greeting
+# ask the user to input the encrypted text
+# Make a decryption class that we will be using for the program
+# we need to initialize the instance variable
+# create a method to decrypt the encrypted text
+# check if the character matches any of the substitution characters, Change '*' to 'a'
+# Change '&' to 'e'
+# Change '#' to 'i'
+# Change '+' to 'o'
+# Change '!' to 'u'
+# Put the character in place if it isn't a replacement character
+# return the decrypted text
+# make a new instance of the Decrypter class using the encrypted text
+# use the decrypter() method and save the decrypted text
+# print out the decrypted text
+# Ask the user if they want to repeat the process
+# If the user doesn't want to repeat the process, exit the loop
